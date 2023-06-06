09692cd29806dc0aae408d26018e8bfbd7f9b800
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-06-06 21:37:41 +0100

    setup

---
+++ 0_setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+cd ~/rds/results/public/gwas/heart_failure/hermes_2020/ftp
+wget https://broad-portal-resources.s3.amazonaws.com/CVDKP/HERMES_Jan2019_HeartFailure_summary_data.README.txt
+wget https://personal.broadinstitute.org/ryank/HERMES_Jan2019_HeartFailure_summary_data.txt.zip
+cat <(gunzip -c HERMES_Jan2019_HeartFailure_summary_data.txt.zip | head -1) \
+    <(gunzip -c HERMES_Jan2019_HeartFailure_summary_data.txt.zip | sed '1d' | sort -k2,2n -k3,3n) | \
+bgzip -f > HERMES_Jan2019_HeartFailure_summary_data.gz
+tabix -S1 -s2 -b3 -e3 HERMES_Jan2019_HeartFailure_summary_data.gz
+
